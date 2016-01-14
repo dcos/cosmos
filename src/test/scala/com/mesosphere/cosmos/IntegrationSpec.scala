48cb1ebda5bcd57afc1d1c02a011f2fc295080d9
BenWhitehead <ben.whitehead@mesosphere.io>
2016-01-14 15:45:58 -0800

    Split integration and unit tests into separate source roots.
    A new src/it root exists that contains integration tests. The
    integration test config extends from the base unit test config and
    allows tests in the integration test directory to leverage code from the
    unit tests.

--- src/test/scala/com/mesosphere/cosmos/IntegrationSpec.scala
+++ src/test/scala/com/mesosphere/cosmos/IntegrationSpec.scala
@@ -1,6 +1,5 @@
 package com.mesosphere.cosmos
 
-import com.twitter.app.{FlagParseException, FlagUsageError, Flags}
 import com.twitter.finagle.Service
 import com.twitter.finagle.http.{Request, Response}
 import io.finch.test.ServiceIntegrationSuite
@@ -11,20 +10,7 @@ abstract class IntegrationSpec
   with ServiceIntegrationSuite
   with CosmosSpec {
 
-  private val failfastOnFlagsNotParsed = false
-  private val name: String = getClass.getName.stripSuffix("$")
-  private val flag: Flags = new Flags(name, includeGlobal = true, failfastOnFlagsNotParsed)
-
   def createService: Service[Request, Response] = {
-    flag.parseArgs(args = Array(), allowUndefinedFlags = true) match {
-      case Flags.Ok(remainder) =>
-        // no-op
-      case Flags.Help(usage) =>
-        throw FlagUsageError(usage)
-      case Flags.Error(reason) =>
-        throw FlagParseException(reason)
-    }
-
     val adminRouterUri = dcosHost()
     val dcosClient = Services.adminRouterClient(adminRouterUri)
     val adminRouter = new AdminRouter(adminRouterUri, dcosClient)
