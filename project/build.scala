e600524c1d9ea616b9623c98fcb57a07e5580028
BenWhitehead <ben.whitehead@mesosphere.io>
2016-04-12 20:15:12 -0700

    Prepare for release of v0.1.5

--- project/build.scala
+++ project/build.scala
@@ -7,7 +7,7 @@ import sbt._
 object CosmosBuild extends Build {
 
   lazy val projectScalaVersion = "2.11.7"
-  lazy val projectVersion = "0.1.5-SNAPSHOT"
+  lazy val projectVersion = "0.1.5"
 
   object V {
     val circe = "0.2.1"
