eaef7c7f0b76729dbdba7e3b062a25cc8b5eca30
Charles Ruhland <cruhland@alumni.cmu.edu>
2016-06-13 15:26:40 -0700

    Implement PackageDescribeHandler for the Universe v3 model (#364)

--- cosmos-server/src/main/scala/com/mesosphere/cosmos/repository/PackageCollection.scala
+++ cosmos-server/src/main/scala/com/mesosphere/cosmos/repository/PackageCollection.scala
@@ -1,7 +1,7 @@
 package com.mesosphere.cosmos.repository
 
-import com.mesosphere.cosmos.rpc.v1.model.SearchResult
-import com.mesosphere.universe.v2.model.{PackageDetailsVersion, PackageFiles, UniverseIndexEntry}
+import com.mesosphere.cosmos.rpc
+import com.mesosphere.universe
 import com.twitter.util.Future
 
 /** An aggregation of packages, possibly from multiple repositories.
@@ -15,11 +15,21 @@ trait PackageCollection {
 
   def getPackageByPackageVersion(
     packageName: String,
-    packageVersion: Option[PackageDetailsVersion])
-  : Future[PackageFiles]
+    packageVersion: Option[universe.v2.model.PackageDetailsVersion])
+  : Future[universe.v2.model.PackageFiles]
 
-  def getPackageIndex(packageName: String): Future[UniverseIndexEntry]
+  def getPackageIndex(packageName: String): Future[universe.v2.model.UniverseIndexEntry]
 
-  def search(query: Option[String]): Future[List[SearchResult]]
+  def search(query: Option[String]): Future[List[rpc.v1.model.SearchResult]]
+
+}
+
+// TODO (version): Rename to PackageCollection
+trait V3PackageCollection {
+
+  def getPackageByPackageVersion(
+    packageName: String,
+    packageVersion: Option[universe.v3.model.PackageDefinition.Version]
+  ): Future[universe.v3.model.V3Package]
 
 }
