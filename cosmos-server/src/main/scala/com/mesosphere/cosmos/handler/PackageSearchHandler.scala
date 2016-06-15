3e7daff09e1f413612f2c9b4cf11bd44ae19fcb6
tamarrow <tamar@mesosphere.io>
2016-06-15 11:45:13 -0700

    universe v3 equivalent of PackageSearchHandler (#375)

--- cosmos-server/src/main/scala/com/mesosphere/cosmos/handler/PackageSearchHandler.scala
+++ cosmos-server/src/main/scala/com/mesosphere/cosmos/handler/PackageSearchHandler.scala
@@ -1,7 +1,7 @@
 package com.mesosphere.cosmos.handler
 
 import com.mesosphere.cosmos.http.RequestSession
-import com.mesosphere.cosmos.repository.PackageCollection
+import com.mesosphere.cosmos.repository.{PackageCollection, V3PackageCollection}
 import com.mesosphere.cosmos.rpc.v1.model.{SearchRequest, SearchResponse}
 import com.twitter.util.Future
 
@@ -18,4 +18,20 @@ private[cosmos] final class PackageSearchHandler(
     }
   }
 
+}
+
+// TODO (version): Rename to PackageSearchHandler
+private[cosmos] final class V3PackageSearchHandler(
+  packageCache: V3PackageCollection
+) extends EndpointHandler[SearchRequest, SearchResponse] {
+
+  override def apply(request: SearchRequest)(implicit
+    session: RequestSession
+  ): Future[SearchResponse] = {
+    packageCache.search(request.query) map { packages =>
+      val sortedPackages = packages.sortBy(p => (!p.selected.getOrElse(false), p.name.toLowerCase))
+      SearchResponse(sortedPackages)
+    }
+
+  }
 }
