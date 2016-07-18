5f967fe2741e6a3d3c82730397de062b1b775d37
Jesus Larios Murillo <jlariosmurillo@mesosphere.io>
2016-07-18 13:39:04 -0700

    Refactor ListHandler to be readable and testable
    * Split the apply method into sensible steps each yielding a future
    	* get applications
    	* get application to repository associations
    	* Given the repo, get the installation of each application
    
    These methods are bulk operations that are at this point doing a lot of
    calls that yield lots of futures and then aggregate them into one big future.
    In the future these bulk methods could be pushed to the receivers so that
    they are less costly.
    
    The organisation of the code into these three methods also makes the
    code more testable, since given mocks for adminRouter and repositories
    we could unit test them independently.

--- cosmos-server/src/main/scala/com/mesosphere/cosmos/handler/ListHandler.scala
+++ cosmos-server/src/main/scala/com/mesosphere/cosmos/handler/ListHandler.scala
@@ -2,6 +2,7 @@ package com.mesosphere.cosmos.handler
 
 import java.nio.charset.StandardCharsets
 import java.util.Base64
+
 import cats.data.Xor
 import com.mesosphere.cosmos.{AdminRouter, CirceError}
 import com.mesosphere.cosmos.converter.Label._
@@ -9,86 +10,100 @@ import com.mesosphere.cosmos.http.RequestSession
 import com.mesosphere.cosmos.label
 import com.mesosphere.cosmos.label.v1.circe.Decoders._
 import com.mesosphere.cosmos.repository.CosmosRepository
-import com.mesosphere.cosmos.rpc
-import com.mesosphere.universe
 import com.mesosphere.cosmos.converter.Universe.internalPackageDefinitionToInstalledPackageInformation
+import com.mesosphere.cosmos.rpc.v1.model.{Installation, InstalledPackageInformation, ListRequest, ListResponse}
+import com.mesosphere.cosmos.thirdparty.marathon.model.{AppId, MarathonApp}
+import com.mesosphere.universe.v3.model.PackageDefinition.ReleaseVersion
 import com.netaporter.uri.Uri
 import com.netaporter.uri.dsl.stringToUri
 import com.twitter.bijection.Conversion.asMethod
 import com.twitter.util.Future
 import io.circe.parse._
 
-import scala.util.Try
 
 private[cosmos] final class ListHandler(
-    adminRouter: AdminRouter,
-    repositories: (Uri) => Future[Option[CosmosRepository]]
-) extends EndpointHandler[rpc.v1.model.ListRequest, rpc.v1.model.ListResponse] {
+  adminRouter: AdminRouter,
+  repositories: (Uri) => Future[Option[CosmosRepository]]
+) extends EndpointHandler[ListRequest, ListResponse] {
 
-  override def apply(request: rpc.v1.model.ListRequest)(implicit session: RequestSession): Future[rpc.v1.model.ListResponse] = {
-    adminRouter.listApps().flatMap { applications =>
+  private case class App(id: AppId,
+                         pkgName: String,
+                         pkgReleaseVersion: ReleaseVersion,
+                         repoUri: String,
+                         pkgMetadata: Option[String])
 
-      val appData = applications.apps.map { app =>
-        // corner case: packageReleaseVersion will be None if parsing the label fails
-        (app.id, app.packageReleaseVersion, app.packageName, app.packageRepository, app.packageMetadata)
-      }
+  override def apply(request: ListRequest)
+                    (implicit session: RequestSession): Future[ListResponse] = {
+    for {
+      apps <- getApplications(adminRouter, request)
+      repoAssocs <- getRepositoryAssociations(repositories, apps)
+      installs <- getInstallations(repoAssocs)
+    } yield {
+      ListResponse(installs.sortBy(install =>
+        (install.packageInformation.packageDefinition.name, install.appId)))
+    }
+  }
 
-      val filtered = appData.filter {
-        case (appId, Some(releaseVersion), Some(packageName), Some(repositoryUri), metaData) =>
-          request.packageName.forall(_ == packageName) && request.appId.forall(_ == appId)
+  private[this] def getApplications(adminRouter: AdminRouter, request: ListRequest)
+                                   (implicit session: RequestSession): Future[Seq[App]] = {
+
+    def satisfiesRequest(app: MarathonApp): Boolean = {
+      // corner case: packageReleaseVersion will be None if parsing the label fails
+      (app.packageReleaseVersion, app.packageName, app.packageRepository) match {
+        case (Some(_), Some(pkgName), Some(_)) =>
+          request.packageName.forall(_ == pkgName) && request.appId.forall(_ == app.id)
         case _ => false
       }
+    }
 
-      val flattened = filtered.map { x => (x._1, x._2.get, x._3.get, x._4.get, x._5) }
-
-      val installations = flattened.map {
-        case (appId, releaseVersion, packageName, repositoryUri, pkgMetadata) =>
-          attemptResolvePackageMetadata(
-            packageName,
-            releaseVersion.as[Try[universe.v3.model.PackageDefinition.ReleaseVersion]].get,
-            repositoryUri
-          ).map { installPackageInfo =>
-            installPackageInfo.getOrElse {
-              val b64PkgInfo = pkgMetadata.getOrElse("")
-              val pkgInfoBytes = Base64.getDecoder.decode(b64PkgInfo)
-              val pkgInfoString = new String(pkgInfoBytes, StandardCharsets.UTF_8)
-              decodePackageFromMarathon(pkgInfoString).as[rpc.v1.model.InstalledPackageInformation]
-            }
-          } map { installedPackageInfo =>
-            rpc.v1.model.Installation(appId, installedPackageInfo)
-          }
+    adminRouter.listApps().map { response =>
+      response.apps.collect{
+        case app if satisfiesRequest(app) =>
+          App(
+            app.id,
+            app.packageName.get,
+            app.packageReleaseVersion.get,
+            app.packageRepository.get,
+            app.packageMetadata
+          )
       }
-
-      Future.collect(installations)
-    } map { installations =>
-      rpc.v1.model.ListResponse(installations.sortBy(i =>
-        (i.packageInformation.packageDefinition.name ,i.appId)))
     }
   }
 
-  private[this] def decodePackageFromMarathon(
-      pkgInfoString: String
-  ): label.v1.model.PackageMetadata = {
-    decode[label.v1.model.PackageMetadata](pkgInfoString) match {
+  private[this] def getRepositoryAssociations(repositories: (Uri) => Future[Option[CosmosRepository]],
+                                              apps: Seq[App])
+  : Future[Seq[(App, Option[CosmosRepository])]] = {
+    Future.collect {
+      apps.map { app =>
+        repositories(app.repoUri).map(repo => (app, repo))
+      }
+    }
+  }
+
+  private[this] def getInstallations(assocs: Seq[(App, Option[CosmosRepository])])
+                                    (implicit session: RequestSession): Future[Seq[Installation]] = {
+    Future.collect {
+      assocs map {
+        case (app, Some(repo)) =>
+          repo
+            .getPackageByReleaseVersion(app.pkgName, app.pkgReleaseVersion)
+            .map(pkgInfo => Installation(app.id, pkgInfo.as[InstalledPackageInformation]))
+        case (app, None) => Future.value(Installation(app.id, decodeInstalledPackageInformation(app)))
+      }
+    }
+  }
+
+  private[this] def decodeInstalledPackageInformation(app: App): InstalledPackageInformation = {
+    val pkgMetadata = app.pkgMetadata.getOrElse("")
+    val pkgInfo = new String(
+      Base64.getDecoder.decode(pkgMetadata),
+      StandardCharsets.UTF_8
+    )
+    decode[label.v1.model.PackageMetadata](pkgInfo) match {
       case Xor.Left(err) => throw new CirceError(err)
-      case Xor.Right(pkg) => pkg
+      case Xor.Right(pkg) => pkg.as[InstalledPackageInformation]
     }
   }
 
-  private[this] def attemptResolvePackageMetadata(
-      packageName: String,
-      releaseVersion: universe.v3.model.PackageDefinition.ReleaseVersion,
-      repositoryUri: Uri
-  )(implicit session: RequestSession)
-    : Future[Option[rpc.v1.model.InstalledPackageInformation]] = {
-    repositories(repositoryUri).flatMap {
-      case Some(repository) =>
-        repository
-          .getPackageByReleaseVersion(packageName, releaseVersion)
-          .map { pkg =>
-            Some(pkg.as[rpc.v1.model.InstalledPackageInformation])
-          }
-      case _ => Future.value(None)
-    }
-  }
 }
+
