946f31451dc11c2fbfa050633549ac18a1dfcfb9
Ken Sipe <kensipe@gmail.com>
2016-07-27 13:21:49 -0500

    adding metronome capability to cosmos

--- cosmos-server/src/main/scala/com/mesosphere/cosmos/handler/CapabilitiesHandler.scala
+++ cosmos-server/src/main/scala/com/mesosphere/cosmos/handler/CapabilitiesHandler.scala
@@ -8,7 +8,7 @@ private[cosmos] final class CapabilitiesHandler
   extends EndpointHandler[Unit, CapabilitiesResponse] {
 
   private[this] val response = CapabilitiesResponse(
-    List(Capability("PACKAGE_MANAGEMENT"), Capability("SUPPORT_CLUSTER_REPORT")))
+    List(Capability("PACKAGE_MANAGEMENT"), Capability("SUPPORT_CLUSTER_REPORT"), Capability("METRONOME")))
 
   override def apply(v1: Unit)(implicit
     session: RequestSession
