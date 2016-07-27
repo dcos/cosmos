9f5c63150a5c531d4bd6f9f0ae19e9282d0602e5
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-07-27 11:00:23 -0700

    spacing

--- cosmos-server/src/test/scala/com/mesosphere/cosmos/TrysSpec.scala
+++ cosmos-server/src/test/scala/com/mesosphere/cosmos/TrysSpec.scala
@@ -4,7 +4,6 @@ import com.mesosphere.cosmos
 import org.scalatest.FreeSpec
 import com.twitter.util.{Try,Return,Throw}
 
-
 final class TrysSpec extends FreeSpec {
 
   "join[A,B]" in {
@@ -15,6 +14,7 @@ final class TrysSpec extends FreeSpec {
     assertResult(Throw(e))(Trys.join(Return(1), Throw(e)))
     assertResult(Throw(n))(Trys.join(Throw(n), Throw(e)))
   }
+
   "join[A,B,C]" in {
     assertResult(Return((1,2,3)))(Trys.join(Return(1), Return(2), Return(3)))
     val e = new IllegalArgumentException
@@ -23,5 +23,4 @@ final class TrysSpec extends FreeSpec {
     assertResult(Throw(e))(Trys.join(Return(1), Throw(e), Return(3)))
     assertResult(Throw(n))(Trys.join(Throw(n), Throw(e), Return(3)))
   }
-
 }
