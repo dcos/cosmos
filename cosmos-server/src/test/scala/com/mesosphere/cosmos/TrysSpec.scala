feca1186f05e0e1746a90f65cd7021aa560e2a1b
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-07-27 10:56:16 -0700

    tests for trys, verify that its left associative

---
+++ cosmos-server/src/test/scala/com/mesosphere/cosmos/TrysSpec.scala
@@ -0,0 +1,27 @@
+package com.mesosphere.cosmos
+
+import com.mesosphere.cosmos
+import org.scalatest.FreeSpec
+import com.twitter.util.{Try,Return,Throw}
+
+
+final class TrysSpec extends FreeSpec {
+
+  "join[A,B]" in {
+    assertResult(Return((1,2)))(Trys.join(Return(1), Return(2)))
+    val e = new IllegalArgumentException
+    val n = new NoSuchElementException
+    assertResult(Throw(e))(Trys.join(Throw(e), Return(2)))
+    assertResult(Throw(e))(Trys.join(Return(1), Throw(e)))
+    assertResult(Throw(n))(Trys.join(Throw(n), Throw(e)))
+  }
+  "join[A,B,C]" in {
+    assertResult(Return((1,2,3)))(Trys.join(Return(1), Return(2), Return(3)))
+    val e = new IllegalArgumentException
+    val n = new NoSuchElementException
+    assertResult(Throw(e))(Trys.join(Throw(e), Return(2), Return(3)))
+    assertResult(Throw(e))(Trys.join(Return(1), Throw(e), Return(3)))
+    assertResult(Throw(n))(Trys.join(Throw(n), Throw(e), Return(3)))
+  }
+
+}
