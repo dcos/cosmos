4a40f88a456bfa0fd2e5e28ed75d39692a200d19
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-07-29 16:09:30 -0700

    more tests

--- cosmos-server/src/test/scala/com/mesosphere/cosmos/repository/MultiRepositorySpec.scala
+++ cosmos-server/src/test/scala/com/mesosphere/cosmos/repository/MultiRepositorySpec.scala
@@ -48,7 +48,7 @@ final class MultiRepositorySpec extends FreeSpec {
       val c = new MultiRepository(storage, client)
       assertResult(Return(cls))(Try(Await.result(c.getPackagesByPackageName("minimal"))))
     }
-    "not in repo" in {
+    "invalid repo" in {
       val invalid = List(PackageRepository("invalid", Uri.parse("/invalid")))
       val repos = List(PackageRepository("minimal", Uri.parse("/test")))
       val storage = TestStorage(repos)
@@ -57,13 +57,13 @@ final class MultiRepositorySpec extends FreeSpec {
       val c = new MultiRepository(storage, client)
       assertResult(Throw(new PackageNotFound("minimal")))(Try(Await.result(c.getPackagesByPackageName("minimal"))))
     }
-    "wrong name" in {
+    "wrong query" in {
       val repos = List(PackageRepository("valid", Uri.parse("/valid")))
       val storage = TestStorage(repos)
-      val cls = List(TestUtil.MaximalPackageDefinition)
+      val cls = List(TestUtil.MinimalPackageDefinition)
       val client = TestClient(repos, cls)
       val c = new MultiRepository(storage, client)
-      assertResult(Throw(new PackageNotFound("minimal")))(Try(Await.result(c.getPackagesByPackageName("minimal"))))
+      assertResult(Throw(new PackageNotFound("MAXIMAL")))(Try(Await.result(c.getPackagesByPackageName("MAXIMAL"))))
     }
     "from many" in {
       val repos = List(PackageRepository("valid", Uri.parse("/valid")))
@@ -73,6 +73,68 @@ final class MultiRepositorySpec extends FreeSpec {
       val c = new MultiRepository(storage, client)
       val expect = List(TestUtil.MinimalPackageDefinition)
       assertResult(Return(expect))(Try(Await.result(c.getPackagesByPackageName("minimal"))))
+    }
+  }
+  "getPackageByPackageVersion" - {
+    "not found" in {
+      val c = new MultiRepository(TestStorage(), TestClient())
+      val ver = TestUtil.MinimalPackageDefinition.version
+      assertResult(Throw(new VersionNotFound("test", ver)))(Try(Await.result(c.getPackageByPackageVersion("test", Some(ver)))))
+      assertResult(Throw(new PackageNotFound("test")))(Try(Await.result(c.getPackageByPackageVersion("test", None))))
+    }
+    "found minimal" in {
+      val u = Uri.parse("/test")
+      val repos = List(PackageRepository("minimal", u))
+      val storage = TestStorage(repos)
+      val cls = List(TestUtil.MinimalPackageDefinition)
+      val client = TestClient(repos, cls)
+      val c = new MultiRepository(storage, client)
+      val ver = TestUtil.MinimalPackageDefinition.version
+      assertResult(Return((cls.head, u)))(Try(Await.result(c.getPackageByPackageVersion("minimal", None))))
+      assertResult(Return((cls.head, u)))(Try(Await.result(c.getPackageByPackageVersion("minimal", Some(ver)))))
+    }
+    "invalid repo" in {
+      val invalid = List(PackageRepository("invalid", Uri.parse("/invalid")))
+      val repos = List(PackageRepository("minimal", Uri.parse("/test")))
+      val storage = TestStorage(repos)
+      val cls = List(TestUtil.MinimalPackageDefinition)
+      val client = TestClient(invalid, cls)
+      val c = new MultiRepository(storage, client)
+      val ver = TestUtil.MinimalPackageDefinition.version
+      assertResult(Throw(new PackageNotFound("minimal")))(Try(Await.result(c.getPackageByPackageVersion("minimal", None))))
+      assertResult(Throw(new VersionNotFound("minimal",ver)))(Try(Await.result(c.getPackageByPackageVersion("minimal", Some(ver)))))
+    }
+    "wrong query" in {
+      val repos = List(PackageRepository("valid", Uri.parse("/valid")))
+      val storage = TestStorage(repos)
+      val cls = List(TestUtil.MinimalPackageDefinition)
+      val client = TestClient(repos, cls)
+      val c = new MultiRepository(storage, client)
+      val ver = TestUtil.MinimalPackageDefinition.version
+      val badver = TestUtil.MaximalPackageDefinition.version
+      assertResult(Throw(new PackageNotFound("MAXIMAL")))(Try(Await.result(c.getPackageByPackageVersion("MAXIMAL", None))))
+      assertResult(Throw(new VersionNotFound("MAXIMAL", ver)))(Try(Await.result(c.getPackageByPackageVersion("MAXIMAL", Some(ver)))))
+      assertResult(Throw(new VersionNotFound("minimal", badver)))(Try(Await.result(c.getPackageByPackageVersion("minimal", Some(badver)))))
+    }
+    "from many" in {
+      val u = Uri.parse("/valid")
+      val repos = List(PackageRepository("valid", u))
+      val storage = TestStorage(repos)
+      val cls = List(TestUtil.MaximalPackageDefinition, TestUtil.MinimalPackageDefinition)
+      val client = TestClient(repos, cls)
+      val c = new MultiRepository(storage, client)
+      val minver = TestUtil.MinimalPackageDefinition.version
+      val minexp = (TestUtil.MinimalPackageDefinition, u)
+      val maxver = TestUtil.MaximalPackageDefinition.version
+
+      assertResult(Return(minexp))(Try(Await.result(c.getPackageByPackageVersion("minimal", None))))
+      assertResult(Return(minexp))(Try(Await.result(c.getPackageByPackageVersion("minimal", Some(minver)))))
+      assertResult(Throw(new VersionNotFound("minimal", maxver)))(Try(Await.result(c.getPackageByPackageVersion("minimal", Some(maxver)))))
+
+      val maxexp = (TestUtil.MaximalPackageDefinition, u)
+      assertResult(Return(maxexp))(Try(Await.result(c.getPackageByPackageVersion("MAXIMAL", None))))
+      assertResult(Return(maxexp))(Try(Await.result(c.getPackageByPackageVersion("MAXIMAL", Some(maxver)))))
+      assertResult(Throw(new VersionNotFound("MAXIMAL", minver)))(Try(Await.result(c.getPackageByPackageVersion("MAXIMAL", Some(minver)))))
     }
   }
 }
