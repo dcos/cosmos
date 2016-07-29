0d66e3775b247080d951f5c03ba9ff4bb08aef66
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-07-29 15:05:00 -0700

    found test

--- cosmos-server/src/test/scala/com/mesosphere/cosmos/repository/MultiRepositorySpec.scala
+++ cosmos-server/src/test/scala/com/mesosphere/cosmos/repository/MultiRepositorySpec.scala
@@ -7,6 +7,7 @@ import com.mesosphere.cosmos.repository.UniverseClient
 import com.mesosphere.cosmos.rpc.v1.model.PackageRepository
 import org.scalatest.FreeSpec
 import com.mesosphere.cosmos.http.RequestSession
+import com.mesosphere.cosmos.test.TestUtil
 import com.mesosphere.cosmos.test.TestUtil.Anonymous //RequestSession implicit
 import com.twitter.util.{Future,Await,Throw,Try,Return}
 import com.netaporter.uri.Uri
@@ -15,9 +16,9 @@ import cats.data.Ior.{Left,Right,Both}
 
 final class MultiRepositorySpec extends FreeSpec {
 
-  case class TestClient(ls: List[PackageDefinition] = Nil) extends UniverseClient {
-    def apply(repository: PackageRepository)(implicit session: RequestSession): Future[CosmosInternalRepository] = Future { 
-      CosmosInternalRepository(ls)
+  case class TestClient(repos: List[PackageRepository] = Nil, ls: List[PackageDefinition] = Nil) extends UniverseClient {
+    def apply(repo: PackageRepository)(implicit session: RequestSession): Future[CosmosInternalRepository] = Future { 
+      CosmosInternalRepository(repos.filter( _ == repo).flatMap(_ => ls))
     }
   }
   case class TestStorage(initial:List[PackageRepository] = Nil) extends PackageSourcesStorage {
@@ -26,28 +27,22 @@ final class MultiRepositorySpec extends FreeSpec {
 
     def readCache(): Future[List[PackageRepository]] =  Future { cache }
 
-    def add(index: Option[Int], packageRepository: PackageRepository): Future[List[PackageRepository]] = {
-      val pos = (index orElse Some(0)).get
-      val (before, after) = cache.splitAt(pos)
-      cache = before ++ (packageRepository :: after)
-      Future { cache }
-    }
-    def delete(nameOrUri: Ior[String, Uri]): Future[List[PackageRepository]] =  {
-      cache = cache.filter { pr => 
-        val PackageRepository(name, uri) = pr
-        nameOrUri match {
-          case (Left(n))    => !(name == n)
-          case (Right(u))   => !(uri == u)
-          case (Both(n,u))  => !(uri == u && name == n)
-        }
-      }
-      Future { cache }
-    }
+    def add(index: Option[Int], packageRepository: PackageRepository): Future[List[PackageRepository]] = sys.error("add")
+
+    def delete(nameOrUri: Ior[String, Uri]): Future[List[PackageRepository]] =  sys.error("delete")
   }
   "getPackagesByPackageName" - {
     "not found" in {
       val c = new MultiRepository(TestStorage(), TestClient())
       assertResult(Throw(new PackageNotFound("test")))(Try(Await.result(c.getPackagesByPackageName("test"))))
+    }
+    "found minimal" in {
+      val repos = List(PackageRepository("minimal", Uri.parse("/test")))
+      val storage = TestStorage(repos)
+      val cls = List(TestUtil.MinimalPackageDefinition)
+      val client = TestClient(repos, cls)
+      val c = new MultiRepository(storage, client)
+      assertResult(Return(cls))(Try(Await.result(c.getPackagesByPackageName("minimal"))))
     }
   }
 }
