2cdbd5f72c39cf37f5d452c0c8bdc382f4865837
Tamar Ben-Shachar <tamar@mesosphere.io>
2016-09-09 13:13:12 -0700

    pr comments

--- cosmos-server/src/main/scala/com/mesosphere/cosmos/storage/DistributedAtomicCounter.scala
+++ cosmos-server/src/main/scala/com/mesosphere/cosmos/storage/DistributedAtomicCounter.scala
@@ -1,6 +1,8 @@
 package com.mesosphere.cosmos.storage
 
+import com.twitter.finagle.stats.{NullStatsReceiver, Stat, StatsReceiver}
 import com.twitter.util.Future
+import com.twitter.util.FuturePool
 
 import org.apache.curator.framework.CuratorFramework
 import org.apache.curator.framework.recipes.atomic.DistributedAtomicLong
@@ -10,20 +12,39 @@ import com.mesosphere.cosmos.ZooKeeperStorageError
 
 private[cosmos] final class DistributedAtomicCounter(
   zkClient: CuratorFramework
+)(implicit
+  statsReceiver: StatsReceiver = NullStatsReceiver
 ) {
 
-  private[this] val counterPath: String = "/package/counter"
-  private[this] val retryPolicy = new ExponentialBackoffRetry(1000, 3)
+  import DistributedAtomicCounter._
+
+  private[this] val pool = FuturePool.interruptibleUnboundedPool
+
   private[this] val counter = new DistributedAtomicLong(zkClient, counterPath, retryPolicy)
 
+  private[this] val stats = statsReceiver.scope("distributedCounter")
+
   def incrementAndGet: Future[Long] = {
-    Future {
-      val newValue = counter.increment()
-      if (newValue.succeeded()) {
-        newValue.postValue()
-      } else {
-        throw new ZooKeeperStorageError("Could not acquire new counter value")
+    Stat.timeFuture(stats.stat("call")) {
+      pool {
+        val newValue = counter.increment()
+
+        val newStats = newValue.getStats()
+        stats.counter("optimisticTries").incr(newStats.getOptimisticTries())
+
+        if (newValue.succeeded()) {
+          newValue.postValue()
+        } else {
+          throw new ZooKeeperStorageError("Could not acquire new counter value")
+        }
       }
     }
   }
+}
+
+private object DistributedAtomicCounter {
+
+  private val counterPath: String = "/package/counter"
+  private val retryPolicy = new ExponentialBackoffRetry(1000, 3)
+
 }
