1c6fb45cbeb436cdfd68832f81921bc83eaff61e
Jesus Larios Murillo <jlariosmurillo@mesosphere.io>
2016-09-14 11:47:01 -0700

    Rename ZooKeeperStorage to ZkRepositoryList

--- cosmos-server/src/test/scala/com/mesosphere/cosmos/repository/ZkRepositoryListSpec.scala
+++ cosmos-server/src/test/scala/com/mesosphere/cosmos/repository/ZkRepositoryListSpec.scala
@@ -5,29 +5,29 @@ import com.mesosphere.cosmos.rpc.v1.model.PackageRepository
 import com.netaporter.uri.dsl._
 import org.scalatest.FreeSpec
 
-class ZooKeeperStorageSpec extends FreeSpec {
+class ZkRepositoryListSpec extends FreeSpec {
 
   val real = PackageRepository("real", "http://real.real")
   val fake = PackageRepository("fake", "http://fake.fake")
   val realFake = PackageRepository("real", "http://fake.fake")
 
-  "ZooKeeperStorage" - {
+  "ZkRepositoryList" - {
     "getPredicate should" - {
       "work when" - {
         "only name is specified" in {
-          val predicate = ZooKeeperStorage.getPredicate(Ior.Left("real"))
+          val predicate = ZkRepositoryList.getPredicate(Ior.Left("real"))
           assert(predicate(real))
           assert(!predicate(fake))
           assert(predicate(realFake))
         }
         "only uri is specified" in {
-          val predicate = ZooKeeperStorage.getPredicate(Ior.Right("http://real.real"))
+          val predicate = ZkRepositoryList.getPredicate(Ior.Right("http://real.real"))
           assert(predicate(real))
           assert(!predicate(fake))
           assert(!predicate(realFake))
         }
         "both name and uri are specified" in {
-          val predicate = ZooKeeperStorage.getPredicate(Ior.Both("real", "http://real.real"))
+          val predicate = ZkRepositoryList.getPredicate(Ior.Both("real", "http://real.real"))
           assert(predicate(real))
           assert(!predicate(fake))
           assert(!predicate(realFake))
