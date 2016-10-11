12f5c84abc055aafe71d14e9e0db7809bce1a422
José Armando García Sancio <jsancio@gmail.com>
2016-10-11 16:55:57 -0700

    Add comment regarding the semantic for delete

--- cosmos-server/src/main/scala/com/mesosphere/cosmos/storage/ObjectStorage.scala
+++ cosmos-server/src/main/scala/com/mesosphere/cosmos/storage/ObjectStorage.scala
@@ -15,10 +15,19 @@ trait ObjectStorage {
     contentLength: Long,
     contentType: Option[MediaType] = None
   ): Future[Unit]
+
   def read(name: String): Future[(Option[MediaType], Reader)]
+
+  /**
+   * Deletes the specified object. If attempting to delete an object that does not exist,
+   * success is returned.
+   */
   def delete(name: String): Future[Unit]
+
   def list(directory: String): Future[ObjectStorage.ObjectList]
+
   def listNext(token: ObjectStorage.ListToken): Future[ObjectStorage.ObjectList]
+
   def getUrl(name: String): Option[Uri]
 }
 
