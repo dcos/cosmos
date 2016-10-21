1a0112732734ec671c170805d79348b5f0c3be8c
Jesus E. Larios Murillo <jlariosmurillo@mesosphere.io>
2016-10-21 16:25:40 -0700

    Add Package Coordinate (#523)
    * Add package coordinate model, coders, injection, and tests

--- cosmos-server/src/main/scala/com/mesosphere/cosmos/converter/Common.scala
+++ cosmos-server/src/main/scala/com/mesosphere/cosmos/converter/Common.scala
@@ -1,10 +1,22 @@
 package com.mesosphere.cosmos.converter
 
+import java.nio.charset.StandardCharsets
+import java.util.Base64
+
+import cats.data.Xor
+import com.mesosphere.cosmos._
 import com.mesosphere.cosmos.internal.model
 import com.mesosphere.cosmos.internal.model.{BundleDefinition, V2Bundle, V3Bundle}
+import com.mesosphere.cosmos.rpc.v1.circe.Decoders._
+import com.mesosphere.cosmos.rpc.v1.circe.Encoders._
+import com.mesosphere.cosmos.rpc.v1.model.PackageCoordinate
 import com.mesosphere.universe
 import com.twitter.bijection.Conversion.asMethod
-import com.twitter.bijection.Bijection
+import com.twitter.bijection.{Bijection, Injection}
+import io.circe.jawn.decode
+import io.circe.syntax._
+
+import scala.util.Try
 
 object Common {
 
@@ -144,5 +156,27 @@ object Common {
     Bijection.build(fwd)(rev)
   }
 
+  implicit val packageCoordinateToBase64String
+  : Injection[PackageCoordinate, String] = {
+    def fwd(coordinate: PackageCoordinate): String = {
+      Base64.getUrlEncoder.encodeToString(
+        coordinate.asJson.noSpaces.getBytes(StandardCharsets.UTF_8)
+      )
+    }
 
+    def rev(str: String): PackageCoordinate = {
+      val coordinate  = new String(
+        Base64.getUrlDecoder.decode(str),
+        StandardCharsets.UTF_8
+      )
+
+      decode[PackageCoordinate](coordinate) match {
+        case Xor.Left(err) => throw CirceError(err)
+        case Xor.Right(c) => c
+      }
+    }
+
+    Injection.build[PackageCoordinate, String](fwd)(s => Try(rev(s)))
+  }
+  
 }
