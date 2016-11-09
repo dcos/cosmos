53945381c9f035aab888589afd35c49d42cc91a6
Jesus E. Larios Murillo <preparedfortherain@gmail.com>
2016-11-09 15:40:28 -0800

    Increase handler readability

--- cosmos-server/src/main/scala/com/mesosphere/cosmos/handler/ServiceStartHandler.scala
+++ cosmos-server/src/main/scala/com/mesosphere/cosmos/handler/ServiceStartHandler.scala
@@ -1,19 +1,19 @@
 package com.mesosphere.cosmos.handler
 
+import _root_.io.circe.syntax._
 import cats.data.Xor
 import com.mesosphere.cosmos._
 import com.mesosphere.cosmos.finch.EndpointHandler
 import com.mesosphere.cosmos.http.RequestSession
 import com.mesosphere.cosmos.render._
 import com.mesosphere.cosmos.repository.LocalPackageCollection
-import com.mesosphere.cosmos.rpc.v1.model.LocalPackage
 import com.mesosphere.cosmos.rpc.v1.model.Installed
+import com.mesosphere.cosmos.rpc.v1.model.LocalPackage
 import com.mesosphere.universe
 import com.mesosphere.universe.bijection.UniverseConversions._
 import com.mesosphere.universe.v3.syntax.PackageDefinitionOps._
 import com.twitter.bijection.Conversion.asMethod
 import com.twitter.util.Future
-import _root_.io.circe.syntax._
 
 private[cosmos] final class ServiceStartHandler(
   localPackageCollection: LocalPackageCollection,
@@ -41,11 +41,8 @@ private[cosmos] final class ServiceStartHandler(
         request.packageVersion.as[Option[universe.v3.model.PackageDefinition.Version]])
       .map(asPackageDefinition(_, request.packageName) -> LocalPackageCollection.Uri)
       .flatMap { case (pkg, sourceUri) =>
-        val packageConfig =
-          PackageDefinitionRenderer
-            .renderMarathonV2App(sourceUri, pkg, request.options, request.appId)
-
-        packageConfig match {
+        PackageDefinitionRenderer
+          .renderMarathonV2App(sourceUri, pkg, request.options, request.appId) match {
           case Xor.Right(renderedMarathonJson) =>
             packageRunner.launch(renderedMarathonJson)
               .map { runnerResponse =>
@@ -55,30 +52,28 @@ private[cosmos] final class ServiceStartHandler(
                   appId = Some(runnerResponse.id)
                 )
               }
-          case Xor.Left(pdre) => pdre match {
-            case OptionsValidationFailure(validationErrors) =>
-              Future.exception(JsonSchemaMismatch(validationErrors))
-            case InvalidLabelSchema(cause) =>
-              Future.exception(CirceError(cause))
-            case RenderedTemplateNotJson(cause) =>
-              Future.exception(CirceError(cause))
-            case RenderedTemplateNotJsonObject =>
-              Future.exception(MarathonTemplateMustBeJsonObject)
-            case OptionsNotAllowed =>
-              val error = Map("message" -> "No schema available to validate the provided options").asJson
-              Future.exception(JsonSchemaMismatch(List(error)))
-            case MissingMarathonV2AppTemplate =>
-              Future {
-                rpc.v2.model.ServiceStartResponse(
-                  packageName = pkg.name,
-                  packageVersion = pkg.version,
-                  appId = None
-                )
-              }
-          }
+          case Xor.Left(MissingMarathonV2AppTemplate) =>
+            Future {
+              rpc.v2.model.ServiceStartResponse(
+                packageName = pkg.name,
+                packageVersion = pkg.version,
+                appId = None
+              )
+            }
+          case Xor.Left(OptionsValidationFailure(validationErrors)) =>
+            Future.exception(JsonSchemaMismatch(validationErrors))
+          case Xor.Left(InvalidLabelSchema(cause)) =>
+            Future.exception(CirceError(cause))
+          case Xor.Left(RenderedTemplateNotJson(cause)) =>
+            Future.exception(CirceError(cause))
+          case Xor.Left(RenderedTemplateNotJsonObject) =>
+            Future.exception(MarathonTemplateMustBeJsonObject)
+          case Xor.Left(OptionsNotAllowed) =>
+            val error = Map("message" -> "No schema available to validate the provided options").asJson
+            Future.exception(JsonSchemaMismatch(List(error)))
         }
       }
   }
-
 }
+
 
