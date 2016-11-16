41d7f4e423fa64444713c22b93172fdd90396a9c
Jesus E. Larios Murillo <preparedfortherain@gmail.com>
2016-11-16 13:08:41 -0800

    Remove wildcard import

--- cosmos-server/src/main/scala/com/mesosphere/cosmos/handler/NotConfiguredHandler.scala
+++ cosmos-server/src/main/scala/com/mesosphere/cosmos/handler/NotConfiguredHandler.scala
@@ -1,6 +1,6 @@
 package com.mesosphere.cosmos.handler
 
-import com.mesosphere.cosmos._
+import com.mesosphere.cosmos.NotImplemented
 import com.mesosphere.cosmos.finch.EndpointHandler
 import com.mesosphere.cosmos.http.RequestSession
 import com.twitter.util.Future
