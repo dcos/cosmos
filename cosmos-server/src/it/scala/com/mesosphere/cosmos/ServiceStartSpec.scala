582bb2c89963ffc0a1943219979769a68fd3afa7
Jesus E. Larios Murillo <jlariosmurillo@mesosphere.io>
2016-12-21 13:52:33 -0800

    Fix error name bug

--- cosmos-server/src/it/scala/com/mesosphere/cosmos/ServiceStartSpec.scala
+++ cosmos-server/src/it/scala/com/mesosphere/cosmos/ServiceStartSpec.scala
@@ -126,7 +126,7 @@ final class ServiceStartSpec extends FreeSpec with InstallQueueFixture with Befo
       assertResult(Some(rpc.MediaTypes.ErrorResponse.show))(startAgainResponse.contentType)
 
       val typedErrorResponse = decode[ErrorResponse](startAgainResponse.contentString)
-      assertResult(classOf[ServiceAlreadyStarted])(typedErrorResponse.`type`)
+      assertResult("ServiceAlreadyStarted")(typedErrorResponse.`type`)
     }
 
   }
