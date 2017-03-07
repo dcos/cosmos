6f9eb473d91c33cd3186923a977856ef6fc1c62c
José Armando García Sancio <jsancio@gmail.com>
2017-03-07 11:12:16 -0800

    Remove TODO after filling issues

--- cosmos-model/src/main/scala/com/mesosphere/cosmos/rpc/v1/model/ServiceStartResponse.scala
+++ cosmos-model/src/main/scala/com/mesosphere/cosmos/rpc/v1/model/ServiceStartResponse.scala
@@ -3,8 +3,6 @@ package com.mesosphere.cosmos.rpc.v1.model
 import com.mesosphere.cosmos.thirdparty.marathon.model.AppId
 import com.mesosphere.universe
 
-// TODO: This should match the response type for update
-// TODO: Why is appId optional?
 case class ServiceStartResponse(
   packageName: String,
   packageVersion: universe.v3.model.PackageDefinition.Version,
