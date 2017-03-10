c5bafe625b4881954c0b238b006d54af52bc1ec9
José Armando García Sancio <jsancio@gmail.com>
2017-03-09 17:06:38 -0800

    Use the old docker commands

--- build/generate_api.sh
+++ build/generate_api.sh
@@ -9,15 +9,15 @@ TARGET_DIR="$REPO_DIR/cosmos-server/target"
 mkdir -p "$TARGET_DIR"
 
 # Build a docker image with raml2html
-sudo docker image build --tag ramltools "$REPO_DIR/docker/ramltools"
+sudo docker build --tag ramltools "$REPO_DIR/docker/ramltools"
 
 # Generate the API HTML using the built docker image
-sudo docker container run --volume="$REPO_DIR":/src:ro --rm ramltools /usr/local/bin/raml2html \
+sudo docker run --volume="$REPO_DIR":/src:ro --rm ramltools /usr/local/bin/raml2html \
   /src/cosmos-server/src/main/resources/com/mesosphere/cosmos/handler/api.raml \
   > "$TARGET_DIR/api.html"
 
 # Generate the API Swagger using the built docker image
-sudo docker container run --volume="$REPO_DIR":/src:ro --rm ramltools /usr/local/bin/raml2swagger \
+sudo docker run --volume="$REPO_DIR":/src:ro --rm ramltools /usr/local/bin/raml2swagger \
   /src/cosmos-server/src/main/resources/com/mesosphere/cosmos/handler/api.raml \
   > "$TARGET_DIR/api.swagger"
 
