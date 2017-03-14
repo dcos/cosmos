99ea67f313613d6218346e2f7c11263b2133c1d6
Charles Ruhland <cruhland@alumni.cmu.edu>
2017-03-14 12:58:50 -0700

    Use sbt-dcos build plugin (#585)

--- project/build.scala
+++ project/build.scala
@@ -1,6 +1,7 @@
 package com.mesosphere.cosmos
 
 import com.github.retronym.SbtOneJar._
+import com.mesosphere.sbt.BuildPlugin
 import sbt.Keys._
 import sbt._
 import scoverage.ScoverageKeys._
@@ -9,13 +10,14 @@ object CosmosBuild {
 
   val teamcityVersion = sys.env.get("TEAMCITY_VERSION")
 
-  val extraSettings = Defaults.coreDefaultSettings
+  val extraSettings = BuildPlugin.publishSettings
 
   val sharedSettings = extraSettings ++ Seq(
     organization := "com.mesosphere.cosmos",
     scalaVersion := V.projectScalaVersion,
     version := V.projectVersion,
 
+    // Required by One-JAR for multi-project builds: https://github.com/sbt/sbt-onejar#requirements
     exportJars := true,
 
     externalResolvers := Seq(
@@ -33,58 +35,16 @@ object CosmosBuild {
       "-Xlint:deprecation"
     ),
 
-    scalacOptions ++= Seq(
-      "-deprecation",            // Emit warning and location for usages of deprecated APIs.
-      "-encoding", "UTF-8",      // Specify character encoding used by source files.
-      "-explaintypes",           // Explain type errors in more detail.
-      "-feature",                // Emit warning for usages of features that should be imported explicitly.
-      "-target:jvm-1.8",         // Target platform for object files.
-      "-unchecked",              // Enable additional warnings where generated code depends on assumptions.
-      "-Xfatal-warnings",        // Fail the compilation if there are any warnings.
-      "-Xfuture",                // Turn on future language features.
-      "-Xlint",                  // Enable or disable specific warnings
-      "-Ywarn-adapted-args",     // Warn if an argument list is modified to match the receiver.
-      "-Ywarn-dead-code",        // Warn when dead code is identified.
-      "-Ywarn-inaccessible",     // Warn about inaccessible types in method signatures.
-      "-Ywarn-infer-any",        // Warn when a type argument is inferred to be `Any`.
-      "-Ywarn-nullary-override", // Warn when non-nullary `def f()' overrides nullary `def f'.
-      "-Ywarn-nullary-unit",     // Warn when nullary methods return Unit.
-      "-Ywarn-numeric-widen",    // Warn when numerics are widened.
-      "-Ywarn-unused",           // Warn when local and private vals, vars, defs, and types are unused.
-      "-Ywarn-unused-import",    // Warn when imports are unused.
-      "-Ywarn-value-discard"     // Warn when non-Unit expression results are unused.
-    ),
-
-    scalacOptions in (Compile, console) ~= (_ filterNot (_ == "-Ywarn-unused-import")),
-
-    scalacOptions in (Test, console) ~= (_ filterNot (_ == "-Ywarn-unused-import")),
-
-    scalacOptions in (Compile, doc) += "-no-link-warnings",
-
-    // Publishing options:
-    publishMavenStyle := true,
-
-    pomIncludeRepository := { x => false },
-
     publishArtifact in Test := false,
 
-    parallelExecution in ThisBuild := false,
-
-    parallelExecution in Test := false,
+    // Parallel changes to a shared cluster cause some tests to fail
+    parallelExecution in IntegrationTest := false,
 
     fork := false,
 
     cancelable in Global := true,
 
     coverageOutputTeamCity := teamcityVersion.isDefined,
-
-    publishTo := {
-      val nexus = "https://oss.sonatype.org/"
-      if (isSnapshot.value)
-        Some("snapshots" at nexus + "content/repositories/snapshots")
-      else
-        Some("releases" at nexus + "service/local/staging/deploy/maven2")
-    },
 
     pomExtra :=
         <url>https://dcos.io</url>
