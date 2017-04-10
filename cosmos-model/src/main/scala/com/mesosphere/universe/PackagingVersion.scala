7d50d3f76b373fa7a404eb8fb7f0625514854c40
Jesus E. Larios Murillo <jlariosmurillo@mesosphere.io>
2017-04-09 22:51:00 -0700

    Make PackagingVersion and PackageDefinition sealed traits (#599)
    * Make PackagingVersion and PackageDefinition sealed traits

--- cosmos-model/src/main/scala/com/mesosphere/universe/PackagingVersion.scala
+++ cosmos-model/src/main/scala/com/mesosphere/universe/PackagingVersion.scala
@@ -9,15 +9,24 @@ import io.circe.Decoder
 import io.circe.DecodingFailure
 import io.circe.Encoder
 import io.circe.HCursor
+import io.circe.syntax.EncoderOps
 
 package v3.model {
 
-  sealed abstract class PackagingVersion private[model](val show: String)
+  sealed trait PackagingVersion {
+    val show: String
+  }
 
   object PackagingVersion {
-    val allVersions = Seq(V2PackagingVersion, V3PackagingVersion)
 
-    private[this] val allVersionsString = allVersions.map(_.show).mkString(", ")
+    val allVersions: Seq[PackagingVersion] =
+      Seq(
+        V2PackagingVersion,
+        V3PackagingVersion
+      )
+
+    private[this] val allVersionsString: String =
+      allVersions.map(_.show).mkString(", ")
 
     private[this] val allVersionsIndex: Map[String, PackagingVersion] = {
       allVersions.map(v => v.show -> v).toMap
@@ -32,11 +41,7 @@ package v3.model {
       }
     }
 
-    implicit def encodePackagingVersion[A <: PackagingVersion]: Encoder[A] = {
-      Encoder[String].contramap(version => version.show)
-    }
-
-    implicit val decodeV3PackagingVersion: Decoder[PackagingVersion] = {
+    implicit val decodePackagingVersion: Decoder[PackagingVersion] = {
       Decoder.instance[PackagingVersion] { (c: HCursor) =>
         c.as[String].map(PackagingVersion(_)).flatMap {
           case Return(v) => Right(v)
@@ -45,37 +50,57 @@ package v3.model {
       }
     }
 
-    def packagingVersionSubclassToString[V <: PackagingVersion](
-      expected: V
-    ): Decoder[V] = {
-      Decoder.instance { (c: HCursor) =>
-        c.as[String].flatMap {
-          case expected.show => Right(expected)
-          case x => Left(DecodingFailure(
-            s"Expected value [${expected.show}] for packaging version, but found [$x]",
-            c.history
-          ))
+    implicit val encodePackagingVersion: Encoder[PackagingVersion] = {
+      Encoder.instance { packagingVersion: PackagingVersion =>
+        packagingVersion match {
+          case V2PackagingVersion => V2PackagingVersion.asJson
+          case V3PackagingVersion => V3PackagingVersion.asJson
         }
       }
     }
 
+    def decodePackagingVersionSubclass[V <: PackagingVersion](expected: V): Decoder[V] = {
+      Decoder.instance { c: HCursor =>
+        c.as[String].flatMap {
+          case expected.show => Right(expected)
+          case s => Left(
+            DecodingFailure(
+              s"Expected value [${expected.show}] for packaging version, but found [$s]",
+              c.history
+            )
+          )
+        }
+      }
+    }
   }
 
-  case object V2PackagingVersion extends PackagingVersion("2.0") {
-    implicit val encodeV3V2PackagingVersion: Encoder[V2PackagingVersion.type] = {
-      PackagingVersion.encodePackagingVersion
+  case object V2PackagingVersion extends PackagingVersion {
+
+    val show: String = "2.0"
+
+    implicit val decodeV2PackagingVersion: Decoder[V2PackagingVersion.type] = {
+      PackagingVersion.decodePackagingVersionSubclass(this)
     }
-    implicit val decodeV3V2PackagingVersion: Decoder[V2PackagingVersion.type] = {
-      PackagingVersion.packagingVersionSubclassToString(V2PackagingVersion)
+
+    implicit val encodeV2PackagingVersion: Encoder[V2PackagingVersion.type] = {
+      Encoder.instance { _: V2PackagingVersion.type =>
+        show.asJson
+      }
     }
   }
 
-  case object V3PackagingVersion extends PackagingVersion("3.0") {
-    implicit val encodeV3V3PackagingVersion: Encoder[V3PackagingVersion.type] = {
-      PackagingVersion.encodePackagingVersion
+  case object V3PackagingVersion extends PackagingVersion {
+
+    val show: String = "3.0"
+
+    implicit val decodeV3PackagingVersion: Decoder[V3PackagingVersion.type] = {
+      PackagingVersion.decodePackagingVersionSubclass(this)
     }
-    implicit val decodeV3V3PackagingVersion: Decoder[V3PackagingVersion.type] = {
-      PackagingVersion.packagingVersionSubclassToString(V3PackagingVersion)
+
+    implicit val encodeV3PackagingVersion: Encoder[V3PackagingVersion.type] = {
+      Encoder.instance { _: V3PackagingVersion.type =>
+        show.asJson
+      }
     }
   }
 
