44e31abb78dc19e729803f39d361fa296d0ade2d
Jesus E. Larios Murillo <jlariosmurillo@mesosphere.io>
2017-06-01 17:08:32 -0700

    Update cosmos documentation (#633)

--- build/generate_api.sh
+++ build/generate_api.sh
@@ -1,32 +1,45 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Used to generate the API HTML
 
 set -e -o pipefail
 
 REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-TARGET_DIR="$REPO_DIR/cosmos-server/target"
+TARGET_DIR="${1:-"$REPO_DIR/target/api"}"
+RAML_DIR="${2:-"$REPO_DIR/docs"}"
+
 mkdir -p "$TARGET_DIR"
 
 # Build a docker image with raml2html
 sudo docker build --tag ramltools "$REPO_DIR/docker/ramltools"
 
-for SERVICE in capabilities service package
+for RAML in $RAML_DIR/*.raml
 do
+  REPO_DIR_DOCKER="/src"
+  RAML_DOCKER="$REPO_DIR_DOCKER/${RAML##"$REPO_DIR/"}"
+
+  BASENAME="$(basename "$RAML")"
+  NAME="${BASENAME%.*}"
+  OUT_RAML="$TARGET_DIR/$NAME.raml"
+  OUT_HTML="$TARGET_DIR/$NAME.html"
+  OUT_SWAGGER="$TARGET_DIR/$NAME.swagger"
+
   # Generate the API HTML using the built docker image
-  echo "Generating $SERVICE.html"
-  sudo docker run --volume="$REPO_DIR":/src:ro --rm ramltools /usr/local/bin/raml2html \
-    "/src/cosmos-server/src/main/resources/com/mesosphere/cosmos/handler/$SERVICE.raml" \
-    > "$TARGET_DIR/$SERVICE.html"
+  echo "Generating $OUT_HTML"
+  sudo docker run \
+    --volume="$REPO_DIR":"$REPO_DIR_DOCKER":ro \
+    --rm ramltools /usr/local/bin/raml2html \
+    "$RAML_DOCKER" > "$OUT_HTML"
 
   # Generate the API Swagger using the built docker image
-  echo "Generating $SERVICE.swagger"
-  sudo docker run --volume="$REPO_DIR":/src:ro --rm ramltools /usr/local/bin/raml2swagger \
-    "/src/cosmos-server/src/main/resources/com/mesosphere/cosmos/handler/$SERVICE.raml" \
-    > "$TARGET_DIR/$SERVICE.swagger"
+  echo "Generating $OUT_SWAGGER"
+  sudo docker run \
+    --volume="$REPO_DIR":"$REPO_DIR_DOCKER":ro \
+    --rm ramltools /usr/local/bin/raml2swagger \
+    "$RAML_DOCKER" > "$OUT_SWAGGER"
 
-  # Copy the API RAML
-  echo "Copying $SERVICE.raml"
-  cp "$REPO_DIR/cosmos-server/src/main/resources/com/mesosphere/cosmos/handler/$SERVICE.raml" \
-    "$TARGET_DIR/$SERVICE.raml"
 done
+
+# Copy the API RAML
+echo "Copying $RAML_DIR to $TARGET_DIR"
+cp -R $RAML_DIR/* $TARGET_DIR
