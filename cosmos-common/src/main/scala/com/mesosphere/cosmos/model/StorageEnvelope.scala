964bf93c2095abf7cfb7f3fd789d66767af591e2
José Armando García Sancio <jsancio@gmail.com>
2017-06-19 18:09:39 -0700

    GZIP Storage data

--- cosmos-common/src/main/scala/com/mesosphere/cosmos/model/StorageEnvelope.scala
+++ cosmos-common/src/main/scala/com/mesosphere/cosmos/model/StorageEnvelope.scala
@@ -10,12 +10,17 @@ import com.mesosphere.universe.common.ByteBuffers
 import com.mesosphere.universe.common.circe.Decoders.decodeByteBuffer
 import com.mesosphere.universe.common.circe.Encoders.encodeByteBuffer
 import com.twitter.finagle.http.Fields
+import com.twitter.io.StreamIO
 import io.circe.Decoder
 import io.circe.Encoder
 import io.circe.generic.semiauto._
 import io.circe.syntax._
+import java.io.ByteArrayOutputStream
+import java.io.ByteArrayInputStream
 import java.nio.ByteBuffer
 import java.nio.charset.StandardCharsets
+import java.util.zip.GZIPInputStream
+import java.util.zip.GZIPOutputStream
 
 final case class StorageEnvelope private (metadata: Map[String, String], data: ByteBuffer) {
   def decodeData[T](implicit mediaTypedDecoder: MediaTypedDecoder[T]): T = {
@@ -27,21 +32,50 @@ final case class StorageEnvelope private (metadata: Map[String, String], data: B
       )
     }
 
+    val storageData = metadata.get(Fields.ContentEncoding).map { encoding =>
+      if (encoding == StorageEnvelope.gzipEncoding) {
+        val byteStream = new ByteArrayOutputStream()
+        val gzipStream = new GZIPInputStream(new ByteArrayInputStream(ByteBuffers.getBytes(data)))
+
+        StreamIO.copy(gzipStream, byteStream)
+
+        byteStream.toByteArray()
+      } else {
+        throw new IllegalArgumentException(
+          "Error while trying to deserialize envelope data. Unknown Content-Encoding: gzip."
+        )
+      }
+    } getOrElse {
+      ByteBuffers.getBytes(data)
+    }
+
     mediaTypedDecode(
-      new String(ByteBuffers.getBytes(data), StandardCharsets.UTF_8),
+      new String(storageData, StandardCharsets.UTF_8),
       contentType
     )
   }
 }
 
 object StorageEnvelope {
+  private val gzipEncoding = "gzip"
 
   def apply[T](data: T)(implicit mediaTypedEncoder: MediaTypedEncoder[T]): StorageEnvelope = {
     val (json, mediaType) = mediaTypedEncoder(data)
 
+    val bytes = {
+      val byteStream = new ByteArrayOutputStream()
+      val gzipStream = new GZIPOutputStream(byteStream)
+      gzipStream.write(json.noSpaces.getBytes(StandardCharsets.UTF_8))
+      gzipStream.close()
+      ByteBuffer.wrap(byteStream.toByteArray())
+    }
+
     StorageEnvelope(
-      metadata = Map(Fields.ContentType -> mediaType.show),
-      data = ByteBuffer.wrap(json.noSpaces.getBytes(StandardCharsets.UTF_8))
+      metadata = Map(
+        Fields.ContentType -> mediaType.show,
+        Fields.ContentEncoding -> gzipEncoding
+      ),
+      data = bytes
     )
   }
 
