376cc416f2e64bc0b81362ca6c9d2209629c549f
Tarun Gupta Akirala <tarugupta.92@gmail.com>
2017-07-11 09:52:33 -0700

    Used circe encoder

--- cosmos-common/src/main/scala/com/mesosphere/cosmos/error/JsonDecodingError.scala
+++ cosmos-common/src/main/scala/com/mesosphere/cosmos/error/JsonDecodingError.scala
@@ -1,16 +1,17 @@
 package com.mesosphere.cosmos.error
 
-import io.circe.Json
+import io.circe.Encoder
 import io.circe.JsonObject
+import io.circe.generic.semiauto.deriveEncoder
 
 final case class JsonDecodingError(
-  typeName : String = "",
+  typeName : String,
   json : String
 ) extends CosmosError {
-  override def data: Option[JsonObject] = Json.fromFields(
-    List(
-      ("typeName",Json.fromString(typeName)),
-      ("string", Json.fromString(json))
-    )).asObject
+  override def data: Option[JsonObject] = CosmosError.deriveData(this)
   override def message: String = s"Unable to decode the JSON value as a ${typeName}"
+}
+
+object JsonDecodingError {
+  implicit val encoder: Encoder[JsonDecodingError] = deriveEncoder
 }
