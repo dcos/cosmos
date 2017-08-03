2d4b61d5dd2e34fc839869b7363120a8132902d5
Jesus E. Larios Murillo <jlariosmurillo@mesosphere.io>
2017-08-02 17:45:45 -0700

    Add final

--- cosmos-integration-tests/src/main/scala/com/mesosphere/cosmos/ServiceDescribeSpec.scala
+++ cosmos-integration-tests/src/main/scala/com/mesosphere/cosmos/ServiceDescribeSpec.scala
@@ -4,13 +4,12 @@ import com.mesosphere.cosmos.ItOps._
 import com.mesosphere.cosmos.error.MarathonAppNotFound
 import com.mesosphere.cosmos.rpc.v1.model.ErrorResponse
 import com.mesosphere.cosmos.thirdparty.marathon.model.AppId
-import com.mesosphere.universe
 import com.twitter.bijection.Conversion.asMethod
 import com.twitter.finagle.http.Status
 import org.scalatest.FeatureSpec
 import org.scalatest.Matchers
 
-class ServiceDescribeSpec extends FeatureSpec with Matchers {
+final class ServiceDescribeSpec extends FeatureSpec with Matchers {
   feature("The service/describe endpoint") {
     scenario("The user would like to know the upgrades available to a service") {
       RoundTrips.withInstallV1("helloworld", Some("0.1.0".detailsVersion)).runWith { ir =>
