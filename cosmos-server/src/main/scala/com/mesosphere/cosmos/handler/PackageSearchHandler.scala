966a256de26c1728d821af4546a259a7244aabd0
Tarun Gupta Akirala <takirala@users.noreply.github.com>
2017-08-04 11:13:25 -0700

    Reimplement MultiRepository (#666)
    - Removed PackageCollection trait and its implementations - CosmosRepository and MultiRepository
    - Added new stand alone class called PackageCollection that uses RepositoryCache for purposes of caching
    - Classes Removed/Added : 
    
        - CosmosRepository.scala
        - MultiRepository.scala
        - PackageCollection.scala (was a trait)
        - MultiRepositorySpec.scala
        - CosmosRepositorySpec.scala
        - PackageSearchHandlerSpec.scala
        + PackageCollection.scala (is a standalone class)
        + PackageCollectionSpec.scala (unit test cases with no wiring. tests only pure functions)
        + RepositoryCache.scala
    
    - Added new unit test cases.

--- cosmos-server/src/main/scala/com/mesosphere/cosmos/handler/PackageSearchHandler.scala
+++ cosmos-server/src/main/scala/com/mesosphere/cosmos/handler/PackageSearchHandler.scala
@@ -7,16 +7,14 @@ import com.mesosphere.cosmos.rpc
 import com.twitter.util.Future
 
 private[cosmos] final class PackageSearchHandler(
-  packageCache: PackageCollection
+  packageCollection: PackageCollection
 ) extends EndpointHandler[rpc.v1.model.SearchRequest, rpc.v1.model.SearchResponse] {
 
-  override def apply(request: rpc.v1.model.SearchRequest)(implicit
-    session: RequestSession
-  ): Future[rpc.v1.model.SearchResponse] = {
-    packageCache.search(request.query) map { packages =>
-      val sortedPackages = packages.sortBy(p => (!p.selected.getOrElse(false), p.name.toLowerCase))
-      rpc.v1.model.SearchResponse(sortedPackages)
-    }
-
+  override def apply(
+    request: rpc.v1.model.SearchRequest
+  )(implicit session: RequestSession): Future[rpc.v1.model.SearchResponse] = {
+    packageCollection
+      .search(request.query)
+      .map(rpc.v1.model.SearchResponse(_))
   }
 }
