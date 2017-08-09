3bf94501ccfc059ac806b3233684df8da40953ff
Tarun Gupta Akirala <tarugupta.92@gmail.com>
2017-08-09 16:58:15 -0700

    Updated error message

--- cosmos-server/src/main/scala/com/mesosphere/cosmos/service/ServiceUninstaller.scala
+++ cosmos-server/src/main/scala/com/mesosphere/cosmos/service/ServiceUninstaller.scala
@@ -36,7 +36,9 @@ final class ServiceUninstaller(
     work.rescue {
       case ex if retries > 0  =>
         if (!ex.isInstanceOf[PredicateDoesNotObtain]) {
-          logger.info(s"Uninstall attempt for $appId didn't finish. $retries retries left.")
+          logger.info(s"Uninstall attempt for $appId didn't finish. $retries retries left.",
+            ex.getClass.getSimpleName,
+            ex.getMessage)
         }
 
         Future.sleep(
