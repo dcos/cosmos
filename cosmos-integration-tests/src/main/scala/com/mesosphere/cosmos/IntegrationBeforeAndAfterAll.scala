a4b88b670280dce86d262cfe15dcd72bd763a750
Tarun Gupta Akirala <takirala@users.noreply.github.com>
2017-08-18 17:26:20 -0700

    Updated integration tests to use the new universe converter service (#684)
    * Updated integration tests to use the new universe converter service
    * Change http to https

--- cosmos-integration-tests/src/main/scala/com/mesosphere/cosmos/IntegrationBeforeAndAfterAll.scala
+++ cosmos-integration-tests/src/main/scala/com/mesosphere/cosmos/IntegrationBeforeAndAfterAll.scala
@@ -7,26 +7,27 @@ import org.scalatest.Suite
 trait IntegrationBeforeAndAfterAll extends BeforeAndAfterAll { this: Suite =>
 
   private[this] val universeUri = "https://downloads.mesosphere.com/universe/02493e40f8564a39446d06c002f8dcc8e7f6d61f/repo-up-to-1.8.json"
+  private[this] val universeConverterUri = "https://universe-converter.mesosphere.com/transform?url=" + universeUri
 
   override def beforeAll(): Unit = {
     Requests.deleteRepository(Some("Universe"))
 
     Requests.addRepository(
       "Universe",
-      universeUri,
+      universeConverterUri,
       Some(0)
     )
 
     val _ = Requests.addRepository(
       "V4TestUniverse",
-      ItObjects.V4TestUniverse,
+      ItObjects.V4TestUniverseConverterURI,
       Some(0)
     )
   }
 
   override def afterAll(): Unit = {
     Requests.deleteRepository(Some("V4TestUniverse"))
-    Requests.deleteRepository(None, Some(universeUri))
+    Requests.deleteRepository(None, Some(universeConverterUri))
     val _ = Requests.addRepository("Universe", "https://universe.mesosphere.com/repo")
   }
 }
