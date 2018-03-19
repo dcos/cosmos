f887b132d2c34618164fc2298255ca9fee18fa1b
José Armando García Sancio <jsancio@users.noreply.github.com>
2018-03-19 13:20:56 -0700

    Make Digest comparable (#725)

--- cosmos-common/src/main/scala/com/mesosphere/security/Digest.scala
+++ cosmos-common/src/main/scala/com/mesosphere/security/Digest.scala
@@ -3,16 +3,20 @@ package com.mesosphere.security
 import fastparse.all._
 import io.netty.buffer.ByteBufUtil
 
-final case class Digest(
+final case class Digest private (
     algorithm: String,
-    digest: Array[Byte]
+    digest: String
 ) {
   override def toString(): String = {
-    s"$algorithm:${ByteBufUtil.hexDump(digest)}"
+    s"$algorithm:$digest}"
   }
 }
 
 object Digest {
+  def apply(algorithm: String, digest: Array[Byte]): Digest = {
+    Digest(algorithm, ByteBufUtil.hexDump(digest))
+  }
+
   def parse(value: String): Option[Digest] = {
     parser.parse(value).fold(
       (_, _, _) => None,
@@ -20,15 +24,13 @@ object Digest {
     )
   }
 
-
   val parser: Parser[Digest] = {
     val algorithm = CharIn('a' to 'z', '0' to '9').rep(1).!
     val digest = CharIn('0' to '9', 'a' to 'f').rep(1).!
 
     P (
       (algorithm ~ ":" ~ digest).map {
-        case (algorithm, digest) =>
-          Digest(algorithm, ByteBufUtil.decodeHexDump(digest))
+        case (algorithm, digest) => Digest(algorithm, digest)
       }
     )
   }
