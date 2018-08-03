e8b383785a50b6db2718fcfa8babc56842b5363d
Tarun Gupta Akirala <takirala@users.noreply.github.com>
2018-08-03 10:58:23 -0700

    Change ResourceProxyHandler to stream data instead of buffering (#735)

--- cosmos-server/src/main/scala/com/mesosphere/cosmos/handler/ResourceProxyHandler.scala
+++ cosmos-server/src/main/scala/com/mesosphere/cosmos/handler/ResourceProxyHandler.scala
@@ -1,8 +1,6 @@
 package com.mesosphere.cosmos.handler
 
-import com.google.common.io.ByteStreams
 import com.mesosphere.cosmos.HttpClient
-import com.mesosphere.cosmos.HttpClient.ResponseData
 import com.mesosphere.cosmos.error.Forbidden
 import com.mesosphere.cosmos.error.GenericHttpError
 import com.mesosphere.cosmos.error.InvalidContentLengthLimit
@@ -12,21 +10,36 @@ import com.mesosphere.cosmos.repository.PackageCollection
 import com.netaporter.uri.Uri
 import com.twitter.finagle.http.Fields
 import com.twitter.finagle.http.Response
+import com.twitter.finagle.http.Status
 import com.twitter.finagle.stats.StatsReceiver
-import com.twitter.io.Buf
+import com.twitter.io.Reader
 import com.twitter.util.Future
 import com.twitter.util.StorageUnit
 import io.finch.Output
 import io.netty.handler.codec.http.HttpResponseStatus
 import java.io.InputStream
+import java.net.HttpURLConnection
 
 final class ResourceProxyHandler private(
   packageCollection: PackageCollection,
   contentLengthLimit: StorageUnit
 )(implicit statsReceiver: StatsReceiver) {
+
   import ResourceProxyHandler._
 
-  def apply(data : (Uri, RequestSession)): Future[Output[Response]] = {
+  class ConnectionClosingInputStream(
+    connection: HttpURLConnection,
+    inputStream: InputStream
+  ) extends InputStream {
+    def read(): Int = inputStream.read()
+
+    override def close(): Unit = {
+      inputStream.close()
+      connection.disconnect()
+    }
+  }
+
+  def apply(data: (Uri, RequestSession)): Future[Output[Response]] = {
     val uri = data._1
     implicit val session = data._2
 
@@ -36,66 +49,38 @@ final class ResourceProxyHandler private(
       }
     }.flatMap { _ =>
       HttpClient
-        .fetch(
+        .fetchStream(
           uri
-        ){ responseData =>
-          val contentBytes = getContentBytes(uri, responseData, contentLengthLimit)
-          val response = Response()
-          response.content = Buf.ByteArray.Owned(contentBytes)
+        ) { (responseData, conn) =>
+          validateContentLength(uri, responseData.contentLength, contentLengthLimit)
+          val response = Response(
+            com.twitter.finagle.http.Version.Http11,
+            Status.Ok,
+            Reader.fromStream(new ConnectionClosingInputStream(conn, responseData.contentStream))
+          )
           response.contentType = responseData.contentType.show
-          response.contentLength = contentBytes.length.toLong
           for (filename <- getFileNameFromUrl(uri)) {
             response.headerMap.add(Fields.ContentDisposition,
               s"""attachment; filename="$filename"""")
           }
-          response
+          Output.payload(response)
         }
-        .map(Output.payload(_))
     }
   }
 }
 
 object ResourceProxyHandler {
 
-  private val EofDetector: Array[Byte] = Array.ofDim(1)
-
   def apply(
     packageCollection: PackageCollection,
     contentLengthLimit: StorageUnit
   )(implicit statsReceiver: StatsReceiver): ResourceProxyHandler = {
-    if(contentLengthLimit.bytes <= 0) {
+    if (contentLengthLimit.bytes <= 0) {
       throw InvalidContentLengthLimit(contentLengthLimit).exception
     }
 
     implicit val handlerScope = statsReceiver.scope("resourceProxyHandler")
     new ResourceProxyHandler(packageCollection, contentLengthLimit)(handlerScope)
-  }
-
-  def getContentBytes(
-    uri: Uri,
-    responseData: ResponseData,
-    contentLengthLimit: StorageUnit
-  ):Array[Byte] = {
-    validateContentLength(uri, responseData.contentLength, contentLengthLimit)
-
-    // Allocate array of ContentLength size
-    // Buffer InputStream into array; if the end is reached but there's more data, fail
-    // If the data runs out before the end of the array, fail
-
-    val Some(length) = responseData.contentLength.map(_.toInt)
-    val contentBytes = Array.ofDim[Byte](length)
-    val bytesRead = bufferFully(responseData.contentStream, contentBytes)
-    if (bytesRead < contentBytes.length) {
-      throw GenericHttpError(uri = uri, clientStatus = HttpResponseStatus.BAD_GATEWAY).exception
-    }
-
-    bufferFully(responseData.contentStream, EofDetector) match {
-      case 0 if responseData.contentLength.isEmpty =>
-        contentBytes.dropRight(contentBytes.length - bytesRead)
-      case x if x > 0 =>
-        throw GenericHttpError(uri = uri, clientStatus = HttpResponseStatus.BAD_GATEWAY).exception
-      case _ => contentBytes
-    }
   }
 
   def getFileNameFromUrl(url: Uri): Option[String] = {
@@ -106,15 +91,11 @@ object ResourceProxyHandler {
     }
   }
 
-  private def bufferFully(is: InputStream, buffer: Array[Byte]): Int = {
-    ByteStreams.read(is, buffer, 0, buffer.length)
-  }
-
-  private def validateContentLength(
+  def validateContentLength(
     uri: Uri,
     contentLength: Option[Long],
     contentLengthLimit: StorageUnit
-  ):Unit = {
+  ): Unit = {
     contentLength match {
       case Some(length) =>
         length match {
