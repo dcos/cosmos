345d8f1bd717f0e266fc24655bd98b5ecc096ab1
Karan Vishwanathan <karanvishwanathan@Mesospheres-MacBook-Pro-44.local>
2018-08-22 14:10:18 -0700

    reenable tests oops

--- cosmos-integration-tests/src/it/scala/com/mesosphere/cosmos/DcosIntegrationTestsSpec.scala
+++ cosmos-integration-tests/src/it/scala/com/mesosphere/cosmos/DcosIntegrationTestsSpec.scala
@@ -3,7 +3,7 @@ package com.mesosphere.cosmos
 import org.scalatest.Suites
 
 final class DcosIntegrationTestsSpec extends Suites(
-//  new AdminRouterClientSpec,
-//  new ServicesIntegrationSpec,
-//  new repository.UniverseClientSpec
+  new AdminRouterClientSpec,
+  new ServicesIntegrationSpec,
+  new repository.UniverseClientSpec
 )
