ccc3cd32bc568557cf33c0ee8ac31720b3f0f173
Tarun Gupta Akirala <tarugupta.92@gmail.com>
2019-06-19 12:41:04 +0530

    Remove the stale application/zip accept header in universe client

--- cosmos-integration-tests/src/main/scala/com/mesosphere/cosmos/handler/PackageRepositorySpec.scala
+++ cosmos-integration-tests/src/main/scala/com/mesosphere/cosmos/handler/PackageRepositorySpec.scala
@@ -111,9 +111,7 @@ class PackageRepositorySpec extends FeatureSpec with Matchers {
       val name = "invalid"
       val uri: Uri = "https://www.google.com/"
       val expectedError = UnsupportedContentType(
-        List(MediaTypes.UniverseV4Repository,
-          MediaTypes.UniverseV3Repository,
-          MediaTypes.UniverseV2Repository),
+        List(MediaTypes.UniverseV4Repository, MediaTypes.UniverseV3Repository),
         Some("text/html;charset=iso-8859-1")
       ).as[ErrorResponse]
       val error = intercept[HttpErrorResponse] {
